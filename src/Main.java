7582aa615705b24c2ffa69fb7f823645f77a83b9
Finn <finn294@gmx.de>
2024-02-27 11:14:06 +0100

    ziel1 start

--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,12 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        //EinlesenDemo.einlesen();
-        //DBVerbindung db = new DBVerbindung();
-        //db.dbVerwenden();
-
-        List<String> files = Utils.openFolder("data/quadrants");
-
-        try (Connection conn = Utils.dbConnectTo("data/cartography.sqlite")) {
-            assert conn != null;
-            conn.createStatement().execute("PRAGMA foreign_keys = ON; PRAGMA foreign_keys;"); // Needed for deletion cascade
-            Statement stmt = conn.createStatement();
-            String anfragestring = "SELECT * FROM Planets;";
-            ResultSet rset = stmt.executeQuery(anfragestring);
-
-            while (rset.next()){
-                Utils.logTS("ID: " + rset.getString("planets_id") + " | Name: " + (rset.getString("name")));
-            }
-            rset.close();
-            stmt.close();
-            conn.close();
-            Utils.logTS("Abgemeldet.\n");
-        } catch (SQLException e) {
-            Utils.logTS("SQL Error at cartography database: " + e);
-            e.printStackTrace();
-        }
-
         // "data/PlanetA-Q1_6x13.txt"
         // "data/PlanetB-Q3_16x50.txt"
         // "data/PlanetB-Q5_50x100.txt"
 
         // setup quadrant
-        Quadrant q = Quadrant.fromFile(files.get(0));
+        Quadrant q = Quadrant.fromFile("data/quadrants/PlanetA-Q1_6x13.txt");
         assert q != null;
         Utils.logTS("Name: " + q.getPlanetName());
         Utils.logTS("Quadrant: Q" + q.getQuadrant());

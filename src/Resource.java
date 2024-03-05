f80d33779d01356746046f79d52ed757f4da93e7
PipInSpace <jupster18@gmx.de>
2024-03-05 10:31:30 +0100

    fix sql statements, remove unused imports

--- src/Resource.java
+++ src/Resource.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * The Resource Class contains a resource type enum and coordinates.
  * It provides methods to convert the type enum to a String for database use;
